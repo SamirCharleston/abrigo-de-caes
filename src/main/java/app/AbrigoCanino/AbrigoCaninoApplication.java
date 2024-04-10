ac079cbd79943bddae845cc88c9ef2b9866ac7ab
LuanMencato <129910915+LuanMencato@users.noreply.github.com>
2024-04-10 20:09:06 -0300

    Commit Inicial.

---
+++ src/main/java/app/AbrigoCanino/AbrigoCaninoApplication.java
@@ -0,0 +1,13 @@
+package app.AbrigoCanino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbrigoCaninoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbrigoCaninoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/AbrigoCanino/AbrigoCaninoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.AbrigoCanino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbrigoCaninoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
