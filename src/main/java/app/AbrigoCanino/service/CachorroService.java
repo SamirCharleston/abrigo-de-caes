89c4302712fce63557fc7a4f23cd9a3c49e12526
LuanMencato <129910915+LuanMencato@users.noreply.github.com>
2024-04-15 21:47:54 -0300

    update

--- src/main/java/app/AbrigoCanino/service/CachorroService.java
+++ src/main/java/app/AbrigoCanino/service/CachorroService.java
@@ -1,4 +1,30 @@
 package app.AbrigoCanino.service;
 
+import app.AbrigoCanino.entities.CachorroEntity;
+import app.AbrigoCanino.repositories.CachorroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CachorroService {
+    @Autowired
+    private CachorroRepository cachorroRepository;
+
+    public List<CachorroEntity> getAllCachorros() {
+        return cachorroRepository.findAll();
+    }
+
+    public CachorroEntity getCachorroById(Long id) {
+        return cachorroRepository.findById(id).orElse(null);
+    }
+
+    public CachorroEntity saveOrUpdateCachorro(CachorroEntity cachorro) {
+        return cachorroRepository.save(cachorro);
+    }
+
+    public void deleteCachorro(Long id) {
+        cachorroRepository.deleteById(id);
+    }
 }
